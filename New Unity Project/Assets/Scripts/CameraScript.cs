59f5cbd7084c3f3d6d34c846c00b39cdf0277248
BigSpice00 <75219014+BigSpice00@users.noreply.github.com>
2020-12-05 20:26:49 +0400

    camera and animation shit

---
+++ New Unity Project/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public bool lockCursor;
+    public float MouseSensitivity = 10;
+    public Transform target;
+    public float dstFromTarget = 4;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    public float rotationSmoothTime = 0.12f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    float yaw;
+    float pitch;
+
+    void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        dstFromTarget -= Input.mouseScrollDelta.y * 0.5f;
+
+        yaw += Input.GetAxis("Mouse X") * MouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * MouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+    }
+}
